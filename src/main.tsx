ce9e0b57b81e7824cbad0c2ef38f7925b094c4f2
Knirta <myknirtak@gmail.com>
2023-12-05 12:46:26 +0200

    add 404.html

--- src/main.tsx
+++ src/main.tsx
@@ -46,7 +46,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/todolist">
           <ThemeProvider theme={theme}>
             <App />
           </ThemeProvider>
